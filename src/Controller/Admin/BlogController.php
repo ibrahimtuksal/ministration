f233e8ce754f16fa09d1cde4158f7957ae0f10f4
ibrahimtuksal <tuksalibrahim@gmail.com>
2022-01-09 21:36:02 +0300

    Add - blog crud

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Blog;
+use App\Form\BlogFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * Class BlogController
  * @package App\Controller\Admin
- * @Route("/admin")
+ * @Route("/admin/blog")
  */
 class BlogController extends AbstractController
 {
@@ -18,19 +25,127 @@ class BlogController extends AbstractController
      * @var EntityManagerInterface
      */
     private EntityManagerInterface $em;
+    /**
+     * @var SluggerInterface
+     */
+    private SluggerInterface $slugger;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, SluggerInterface $slugger)
     {
         $this->em = $entityManager;
+        $this->slugger = $slugger;
     }
 
     /**
-     * @Route("/admin/blog", name="admin_blog")
+     * @Route("/", name="admin_blog")
+     * @Template()
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('admin/blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+        $blog = $this->em->getRepository(Blog::class)->findAll();
+        return [
+            'blogs' => $blog
+        ];
+    }
+
+    /**
+     * @Route("/add", name="admin_blog_add")
+     * @Template()
+     * @param Request $request
+     * @return array|RedirectResponse
+     */
+    public function add(Request $request)
+    {
+        $blog = new Blog();
+        $form = $this->createForm(BlogFormType::class, $blog);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $blog->setSlug($this->slugger->slug($form->get('title')->getData()));
+
+            if ($brochureFile  = $form->get('photo')->getData()){
+                $dir = $this->getParameter('uploads_dir');
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = $this->slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+                try {
+                    $brochureFile->move(
+                        $dir,
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Hata');
+                    throw new UnprocessableEntityHttpException('Dosya Yüklenemedi');
+                }
+                $blog->setPhoto("/uploads/".$newFilename);
+            }
+
+            $this->em->persist($blog);
+            $this->em->flush();
+            $this->addFlash('success', 'Başarıyla Eklendi.');
+            return $this->redirectToRoute('admin_blog');
+        }
+
+        return [
+            'form' => $form->createView()
+        ];
+    }
+
+
+    /**
+     * @Route("/update/{blog}", name="admin_blog_update")
+     * @Template()
+     * @param int $blog
+     * @param Request $request
+     * @return array|RedirectResponse
+     */
+    public function update(int $blog, Request $request)
+    {
+        $blog = $this->em->getRepository(Blog::class)->find($blog);
+        $form = $this->createForm(BlogFormType::class, $blog);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $blog->setSlug($this->slugger->slug($form->get('title')->getData()));
+
+            if ($brochureFile  = $form->get('photo')->getData()){
+                $dir = $this->getParameter('uploads_dir');
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = $this->slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+                try {
+                    $brochureFile->move(
+                        $dir,
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Hata');
+                    throw new UnprocessableEntityHttpException('Dosya Yüklenemedi');
+                }
+                $blog->setPhoto("/uploads/".$newFilename);
+            }
+
+            $this->em->flush();
+            $this->addFlash('success', 'Başarıyla Eklendi.');
+            return $this->redirectToRoute('admin_blog');
+        }
+
+        return [
+            'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * @Route("/delete/{blog}", name="admin_blog_delete")
+     * @param int $blog
+     * @return RedirectResponse
+     */
+    public function delete(int $blog)
+    {
+        $blog = $this->em->getRepository(Blog::class)->find($blog);
+        $this->em->remove($blog);
+        $this->em->flush();
+        $this->addFlash('success',"Marka Silindi");
+        return $this->redirectToRoute('admin_blog');
     }
 }
---
+++ src/Form/BlogFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Blog;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            //->add('slug')
+            ->add('photo', FileType::class,['required' => false, 'mapped' => false])
+            ->add('content', CKEditorType::class, [
+                'label' => 'İçerik',
+                'config' => [
+                    'uiColor' => '#e2e2ee2',
+                    'toolbar' => 'full',
+                    'required' => true
+                ]
+            ])
+            ->add('button', SubmitType::class, [
+                'label' => 'Kaydet',
+                'row_attr' => ['class' => 'd-grid gap-2']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Blog::class,
+        ]);
+    }
+}
