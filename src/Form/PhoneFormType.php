489e0692e187488612a86c5872933a1b995bae71
ibrahimtuksal <tuksalibrahim@gmail.com>
2022-01-09 21:15:47 +0300

    Add - phone form type

---
+++ src/Form/PhoneFormType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Phone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PhoneFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('phone')
+            ->add('whatsapp')
+            ->add('button', SubmitType::class, [
+                'label' => 'Kaydet',
+                'row_attr' => ['class' => 'd-grid gap-2']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Phone::class,
+        ]);
+    }
+}
