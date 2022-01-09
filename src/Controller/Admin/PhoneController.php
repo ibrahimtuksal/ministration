8ab02f41e86d3996799a9ce22b5bbc6e95ca7919
ibrahimtuksal <tuksalibrahim@gmail.com>
2022-01-09 21:15:31 +0300

    Add - phone update

--- src/Controller/Admin/PhoneController.php
+++ src/Controller/Admin/PhoneController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Phone;
+use App\Form\PhoneFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class PhoneController
  * @package App\Controller\Admin
- * @Route("/admin")
+ * @Route("/admin/phone")
  */
 class PhoneController extends AbstractController
 {
+    CONST PHONE_ID = 1;
 
     /**
      * @var EntityManagerInterface
@@ -26,12 +30,21 @@ class PhoneController extends AbstractController
     }
 
     /**
-     * @Route("/admin/phone", name="admin_phone")
+     * @Route("/", name="admin_phone")
+     * @Template()
      */
-    public function index(): Response
+    public function index(Request $request)
     {
-        return $this->render('admin/phone/index.html.twig', [
-            'controller_name' => 'PhoneController',
-        ]);
+        $phone = $this->em->getRepository(Phone::class)->find(self::PHONE_ID);
+        $form = $this->createForm(PhoneFormType::class, $phone);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->em->flush();
+            $this->addFlash('success', 'Başarıyla Kaydedildi.');
+            return $this->redirectToRoute('admin_phone');
+        }
+        return [
+            'form' => $form->createView()
+        ];
     }
 }
