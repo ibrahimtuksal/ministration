68dc29b611b925f31613a9e79ad6a19d88152d4c
İbrahim TUKSAL <ibrahim.tuksal@path.com.tr>
2022-02-10 20:24:27 +0300

    Update - user log

--- src/Service/UserLogService.php
+++ src/Service/UserLogService.php
@@ -25,8 +25,7 @@ class UserLogService
         $userLog = $this->em->getRepository(UserLog::class)->findOneBy(['ip' => $request->getClientIp()], ['id' => 'desc']);
         if ($userLog instanceof UserLog){
             $nowDate = new \DateTime();
-            $nowDate = $nowDate->format('Y-m-d H:i:s');
-            if ($this->dateDifference($userLog->getCreatedAt()->format('Y-m-d H:i:s'), $nowDate) < 24){
+            if ($this->dateDifference($nowDate->format('Y-m-d H:i:s'), $userLog->getCreatedAt()->format('Y-m-d H:i:s') ) < 24){
                 if ($userLog->getCount() < 5){
                     $userLog->setCount($userLog->getCount()+1);
                     $this->em->flush();
@@ -66,13 +65,11 @@ class UserLogService
         return false;
     }
 
-    public function dateDifference($dateOne , $dateTwo , $differenceFormat = '%h' )
+    public function dateDifference(string $nowDate, string $createdAt)
     {
-        $datetime1 = date_create_from_format('Y-m-d H:i:s', $dateOne);
-        $datetime2 = date_create_from_format('Y-m-d H:i:s', $dateTwo);
-
-        $interval = date_diff($datetime1, $datetime2);
-
-        return $interval->format($differenceFormat);
+        $nowDate = strtotime( $nowDate );
+        $createdAt = strtotime( $createdAt );
+        $diff = $nowDate - $createdAt;
+        return $diff / ( 60 * 60 );
     }
 }
