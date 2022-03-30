2db7652f98835fcd7f44eab236272cea70c4b9e8
ibrahimtuksal <tuksalibrahim@gmail.com>
2022-03-30 13:18:10 +0300

    Update - user log disabled

--- src/Service/UserLogService.php
+++ src/Service/UserLogService.php
@@ -29,25 +29,25 @@ class UserLogService
         $userLog = $this->em->getRepository(UserLog::class)->findOneBy(['ip' => $request->getClientIp(), 'is_what' => true], ['id' => 'desc']);
         // kullanıcı reklamdan daha önce girmişse
         if ($userLog instanceof UserLog && $userLog->getIp() == $request->getClientIp() && $request->query->get('ads') === "1"){
-            //ip son haneyi sıfır yap
-            $ip = explode(".", $request->getClientIp());
-            $ip[3] = "0";
-            $ip = implode(".", $ip);
-            //ssh bağlan
-            $connection = (new SSHConnection())
-                ->to($this->parameterBag->get('ssh_url'))
-                ->as($this->parameterBag->get('ssh_as'))
-                ->withPassword($this->parameterBag->get('ssh_pass'))
-                ->connect();
-            // ban yetkisi aç
-            $connection->run('service firewalld start');
-            // banla
-            $connection->run("firewall-cmd --permanent --add-rich-rule=\"rule family='ipv4' source address='$ip/24' reject\"");
-            // onayla
-            $connection->run('firewall-cmd --reload');
+//            //ip son haneyi sıfır yap
+//            $ip = explode(".", $request->getClientIp());
+//            $ip[3] = "0";
+//            $ip = implode(".", $ip);
+//            //ssh bağlan
+//            $connection = (new SSHConnection())
+//                ->to($this->parameterBag->get('ssh_url'))
+//                ->as($this->parameterBag->get('ssh_as'))
+//                ->withPassword($this->parameterBag->get('ssh_pass'))
+//                ->connect();
+//            // ban yetkisi aç
+//            $connection->run('service firewalld start');
+//            // banla
+//            $connection->run("firewall-cmd --permanent --add-rich-rule=\"rule family='ipv4' source address='$ip/24' reject\"");
+//            // onayla
+//            $connection->run('firewall-cmd --reload');
             // banladığını log'a bildir
-            $userLog->setIsBanned(true);
-            $this->smsService->sendSms("Birileri ban yedi hemen ads paneline ip'yi ekle! ip: ".$request->getClientIp());
+//            $userLog->setIsBanned(true);
+//            $this->smsService->sendSms("Birileri ban yedi hemen ads paneline ip'yi ekle! ip: ".$request->getClientIp());
         }else {
             $log = new UserLog();
             $log->setIp($request->getClientIp());
