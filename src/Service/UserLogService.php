8e46e4b82539888239efaa0f67258046a57ad565
ibrahimtuksal <tuksalibrahim@gmail.com>
2022-03-30 13:25:09 +0300

    Update - user log disabled2

--- src/Service/UserLogService.php
+++ src/Service/UserLogService.php
@@ -26,44 +26,56 @@ class UserLogService
 
     public function userLogControl(Request $request)
     {
-        $userLog = $this->em->getRepository(UserLog::class)->findOneBy(['ip' => $request->getClientIp(), 'is_what' => true], ['id' => 'desc']);
-        // kullanıcı reklamdan daha önce girmişse
-        if ($userLog instanceof UserLog && $userLog->getIp() == $request->getClientIp() && $request->query->get('ads') === "1"){
-//            //ip son haneyi sıfır yap
-//            $ip = explode(".", $request->getClientIp());
-//            $ip[3] = "0";
-//            $ip = implode(".", $ip);
-//            //ssh bağlan
-//            $connection = (new SSHConnection())
-//                ->to($this->parameterBag->get('ssh_url'))
-//                ->as($this->parameterBag->get('ssh_as'))
-//                ->withPassword($this->parameterBag->get('ssh_pass'))
-//                ->connect();
-//            // ban yetkisi aç
-//            $connection->run('service firewalld start');
-//            // banla
-//            $connection->run("firewall-cmd --permanent --add-rich-rule=\"rule family='ipv4' source address='$ip/24' reject\"");
-//            // onayla
-//            $connection->run('firewall-cmd --reload');
-            // banladığını log'a bildir
-//            $userLog->setIsBanned(true);
-//            $this->smsService->sendSms("Birileri ban yedi hemen ads paneline ip'yi ekle! ip: ".$request->getClientIp());
-        }else {
-            $log = new UserLog();
-            $log->setIp($request->getClientIp());
-            $log->setCreatedAt(new \DateTime());
-            $log->setAgent($_SERVER['HTTP_USER_AGENT']);
-            $log->setIsBanned(false);
-            if ($request->query->get('ads') === "1")
-            {
-                $log->setIsWhat(true);
-                $this->smsService->sendSms("Reklamdan Giren bir kullanıcı var!");
-            } else {
-                $log->setIsWhat(false);
-            }
-            $this->em->persist($log);
+//        $userLog = $this->em->getRepository(UserLog::class)->findOneBy(['ip' => $request->getClientIp(), 'is_what' => true], ['id' => 'desc']);
+//        // kullanıcı reklamdan daha önce girmişse
+//        if ($userLog instanceof UserLog && $userLog->getIp() == $request->getClientIp() && $request->query->get('ads') === "1"){
+////            //ip son haneyi sıfır yap
+////            $ip = explode(".", $request->getClientIp());
+////            $ip[3] = "0";
+////            $ip = implode(".", $ip);
+////            //ssh bağlan
+////            $connection = (new SSHConnection())
+////                ->to($this->parameterBag->get('ssh_url'))
+////                ->as($this->parameterBag->get('ssh_as'))
+////                ->withPassword($this->parameterBag->get('ssh_pass'))
+////                ->connect();
+////            // ban yetkisi aç
+////            $connection->run('service firewalld start');
+////            // banla
+////            $connection->run("firewall-cmd --permanent --add-rich-rule=\"rule family='ipv4' source address='$ip/24' reject\"");
+////            // onayla
+////            $connection->run('firewall-cmd --reload');
+//            // banladığını log'a bildir
+////            $userLog->setIsBanned(true);
+////            $this->smsService->sendSms("Birileri ban yedi hemen ads paneline ip'yi ekle! ip: ".$request->getClientIp());
+//        }else {
+//            $log = new UserLog();
+//            $log->setIp($request->getClientIp());
+//            $log->setCreatedAt(new \DateTime());
+//            $log->setAgent($_SERVER['HTTP_USER_AGENT']);
+//            $log->setIsBanned(false);
+//            if ($request->query->get('ads') === "1")
+//            {
+//                $log->setIsWhat(true);
+//                $this->smsService->sendSms("Reklamdan Giren bir kullanıcı var!");
+//            } else {
+//                $log->setIsWhat(false);
+//            }
+//            $this->em->persist($log);
+//        }
+        $log = new UserLog();
+        $log->setIp($request->getClientIp());
+        $log->setCreatedAt(new \DateTime());
+        $log->setAgent($_SERVER['HTTP_USER_AGENT']);
+        $log->setIsBanned(false);
+        if ($request->query->get('ads') === "1")
+        {
+            $log->setIsWhat(true);
+            $this->smsService->sendSms("Reklamdan Giren bir kullanıcı var!");
+        } else {
+            $log->setIsWhat(false);
         }
-
+        $this->em->persist($log);
         $this->em->flush();
         return true;
     }
