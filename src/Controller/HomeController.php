955be7d1ad58f4fd1a4bb054f928edb77b0ae340
haliltuksal <tuksalhalil@gmail.com>
2022-12-17 01:42:47 +0300

    Add - all categories userLog

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -38,10 +38,7 @@ class HomeController extends AbstractController
      */
     public function index(UserLogService $userLogService, Request $request, GlobalGenerator $globalGenerator)
     {
-        if ($globalGenerator->general->getIsReturnPhoneForAds() && $request->query->get('ads') == "1"){
-            $userLogService->userLogControl($request);
-            return $this->redirect('tel:05061614265');
-        }
+        $userLogService->userLogControl($request);
         /** @var Corporate $corporateIndex */
         $corporateIndex = $this->em->getRepository(Corporate::class)->findOneBy(['is_index' => true]);
         /** @var Slider $sliders */
